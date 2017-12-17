61c0ffcac43a563db6895ec02aa4c90452347afa
Omniwheelss <omniwheelss@gmail.com>
2017-12-17 08:42:47 +0530

    New vehicle report added for testing purpose

---
+++ vehicle_summary_v1.php
@@ -0,0 +1,32 @@
+  <?php
+	include("header_inner.php");
+  ?>
+
+  <!-- Right side column. Contains the navbar and content of the page -->
+            <aside class="right-side">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Vehicle Summary Report<?php if(isset($_REQUEST['imei'])) echo "- ".$vehicle_nos[$_REQUEST['imei']]?>
+                        <small></small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Home > </a></li> 
+						<i></i> Reports ></a></li>
+                        <li class="active">Vehicle Summary Report</li>
+                    </ol>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+
+					<?php
+						include_once("vehicle_summary_report_v1.php");
+					?>					
+                    
+                </section><!-- /.content -->
+            </aside><!-- /.right-side -->
+        </div><!-- ./wrapper -->
+  <?php
+	include("footer_table.php");
+  ?>
